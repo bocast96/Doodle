c234f86acd384c7fbbaba8cad93234ae92e9a7b4
Boris Castillo <bocast96@terpmail.umd.edu>
2016-11-04 01:45:59 -0400

    Added clear button

--- app/src/main/java/com/example/boris/doodle/DoodleView.java
+++ app/src/main/java/com/example/boris/doodle/DoodleView.java
@@ -1,10 +1,12 @@
 package com.example.boris.doodle;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.PorterDuff;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
@@ -15,8 +17,10 @@ import android.view.View;
 
 public class DoodleView extends View {
 
-    private Paint paint = new Paint();
+    private Paint paint = new Paint(), canvasPaint = new Paint();
     private Path path = new Path();
+    private Canvas canvas;
+    private Bitmap bitmap;
 
     public DoodleView(Context context) {
         super(context);
@@ -37,11 +41,24 @@ public class DoodleView extends View {
         paint.setColor(Color.RED);
         paint.setAntiAlias(true);
         paint.setStyle(Paint.Style.STROKE);
+        canvasPaint.setFlags(Paint.DITHER_FLAG);
 
+    }
+
+    public void clear() {
+        canvas.drawColor(0, PorterDuff.Mode.CLEAR);
+        invalidate();
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        bitmap = Bitmap.createBitmap(w,h,Bitmap.Config.ARGB_8888);
+        canvas = new Canvas(bitmap);
     }
 
     @Override
     public void onDraw (Canvas canvas) {
+        canvas.drawBitmap(bitmap, 0, 0, canvasPaint);
         canvas.drawPath(path, paint);
     }
 
@@ -58,11 +75,14 @@ public class DoodleView extends View {
                 path.lineTo(xPos,yPos);
                 break;
             case MotionEvent.ACTION_UP:
-
+                canvas.drawPath(path, paint);
+                path.reset();
                 break;
         }
 
         invalidate();
         return true;
     }
+
+
 }
--- app/src/main/java/com/example/boris/doodle/MainActivity.java
+++ app/src/main/java/com/example/boris/doodle/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.boris.doodle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void clearClicked (View view) {
+        //System.out.println("Here");
+        DoodleView doodleView = (DoodleView) findViewById(R.id.view);
+        doodleView.clear();
     }
 }
